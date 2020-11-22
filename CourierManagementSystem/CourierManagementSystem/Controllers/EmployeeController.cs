e5ac85c66a0d108ef39a5ca84ee64228efe00422
nursm86 <lucifersm86@gmail.com>
2020-11-22 21:56:51 +0600

    gitignore added

---
+++ CourierManagementSystem/CourierManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourierManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult serviceHistory()
+        {
+            return View();
+        }
+    }
+}
